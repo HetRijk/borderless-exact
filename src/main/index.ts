ced3efb28e10cbe0c51e71fadca5b87bb8acf0b9
Jelle Licht <jlicht@fsfe.org>
2017-04-07 16:34:20 +0200

    Added initial oauth tokens workflow

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,3 +1,39 @@
 import { Calculator } from './models/';
-
+import * as passport from 'passport';
+import * as OAuth2Strategy from 'passport-oauth2';
+import * as express from 'express';
+var app = express()
+import * as eo from 'exact-online';
 export default Calculator;
+
+let CLIENT_ID = 'f5fcbe04-c19e-45c4-8aa5-f9316d70bb0d';
+let CLIENT_SECRET = 'WhKLQKKQjkOY';
+let CB_URL = "http://localhost:3000/auth/callback";
+
+passport.use(new OAuth2Strategy({
+  authorizationURL: 'https://start.exactonline.nl/api/oauth2/auth',
+  tokenURL: 'https://start.exactonline.nl/api/oauth2/token',
+  clientID: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  callbackURL: CB_URL
+}, (accessToken, refreshToken, profile, cb) => {
+  console.log('AT: ',accessToken);
+  console.log('RT: ',refreshToken);
+  console.log('p: ',profile);
+  return cb(null, true);
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.get('/auth',
+        passport.authenticate('oauth2'));
+app.get('/auth/callback',
+        passport.authenticate('oauth2', { failureRedirect: '/login' }),
+        (req: express.Request , res: express.Response) => {
+          // Successful authentication, redirect home.
+          res.redirect('/');
+        });
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
