3b6b9cbd72904d9883a60544ca3dceffff42b4aa
Jelle Licht <jlicht@fsfe.org>
2017-06-15 12:40:32 +0200

    Refactored some code, WE HAZ REFRESH :collision:

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,13 +3,17 @@ import * as passport from 'passport';
 import * as OAuth2Strategy from 'passport-oauth2';
 import * as express from 'express';
 import * as opn from 'opn';
-var app = express()
+const app = express();
 import * as eo from 'exact-online';
+import TokenWrapper from './utils/TokenWrapper';
 export default Calculator;
 
-let CLIENT_ID = 'f5fcbe04-c19e-45c4-8aa5-f9316d70bb0d';
-let CLIENT_SECRET = 'WhKLQKKQjkOY';
-let CB_URL = "http://localhost:3000/auth/callback";
+const CLIENT_ID = 'f5fcbe04-c19e-45c4-8aa5-f9316d70bb0d';
+const CLIENT_SECRET = 'WhKLQKKQjkOY';
+const CB_URL = 'http://localhost:3000/auth/callback';
+const TOKEN_URL = 'https://start.exactonline.nl/api/oauth2/token';
+const tokenWrapper = new TokenWrapper( TOKEN_URL, CLIENT_ID, CLIENT_SECRET);
+
 // TODO: wtf, y we need this.
 passport.serializeUser((user, done) => {
   done(null, user);
@@ -19,18 +23,13 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
-
 passport.use(new OAuth2Strategy({
   authorizationURL: 'https://start.exactonline.nl/api/oauth2/auth',
-  tokenURL: 'https://start.exactonline.nl/api/oauth2/token',
+  tokenURL: TOKEN_URL,
   clientID: CLIENT_ID,
   clientSecret: CLIENT_SECRET,
-  callbackURL: CB_URL
+  callbackURL: CB_URL,
 }, (accessToken, refreshToken, profile, cb) => {
-
-  console.log('AT: ',accessToken);
-  console.log('RT: ',refreshToken);
-  console.log('p: ',profile);
   return cb(null, true);
 }));
 
@@ -41,11 +40,24 @@ app.get('/auth',
 app.get('/auth/callback',
         passport.authenticate('oauth2', { failureRedirect: '/login' }),
         (req: express.Request , res: express.Response) => {
-          // Successful authentication, redirect home.
-          res.redirect('/');
+          res.setHeader('Content-Type', 'application/json');
+          res.send(JSON.stringify({ message: 'auth complete' }));
         });
 
-app.listen(3000, function () {
+app.get('/verify',
+        (req: express.Request , res: express.Response) => {
+          // Successful authentication, redirect home.
+          const tokenLogger = (err: any, token: string) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify({
+              error: err,
+              token,
+            }));
+          };
+          tokenWrapper.getToken(tokenLogger);
+        });
+
+app.listen(3000,  () => {
   console.log('Example app listening on port 3000!')
   opn('http://localhost:3000/auth/');
-})
+});
